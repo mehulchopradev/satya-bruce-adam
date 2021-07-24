be7e9c000d26121f890b015c32a4300e4f0f8688
Mehul Chopra <mehulchopra@Mehuls-MacBook-Pro.local>
2021-07-24 22:49:42 +0530

    day 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Components
+import './App.styles.scss';
+import CalculatorForm from './components/calculator-form/calculator-form.component';
+
+function App() {
+  return (
+    <CalculatorForm firstNo="10" secondNo="4" ans="14"/>
+  );
+}
+
+export default App;
---
+++ src/components/calculator-form/calculator-form.component.jsx
@@ -0,0 +1,89 @@
+import './calculator-form.styles.scss';
+import { Component } from 'react';
+
+class CalculatorForm extends Component {
+  // gets a property called as `props`
+  // get a property called as `state`
+
+  // initial state of the component
+  state = {
+    firstNo: this.props.firstNo,
+    secondNo: this.props.secondNo,
+    ans: this.props.ans,
+    operation: '',
+  }
+
+  // in class level components, event handlers to be defined using es6 arrow syntax
+  handleClick = () => {
+    console.log(this.state);
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const { name, value } = target;
+
+    // always update internal state using this.setState()
+    this.setState({
+      [name]: value,
+    }); // re render the GUI -> again call internally the render()
+  }
+
+  render() {
+    const { firstNo, secondNo } = this.state;
+    const isDisabled = !firstNo || !secondNo || isNaN(parseInt(firstNo)) || isNaN(parseInt(secondNo));
+
+    return (
+      <div className='calculator-container'>
+        <div className='row'>
+          <input type="text" name="firstNo" placeholder="enter the 1st no" value={this.state.firstNo} onChange={this.handleChange}/>
+          <select name="operation" onChange={this.handleChange}>
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+          </select>
+          <input type="text" name="secondNo" placeholder="enter the 2nd no" value={this.state.secondNo} onChange={this.handleChange}/>
+        </div>
+        <div className='row'>
+          <button disabled={isDisabled} onClick={this.handleClick}>Calculate</button>
+        </div>
+        <div className='row'>
+          <input type="text" name="" placeholder="here is the ans!" value={this.state.ans} readOnly/>
+        </div>
+    </div>
+    )
+  }
+}
+
+
+/* function CalculatorForm (props) {
+  const handleClick = () => {
+    // get the value of first no from the internal component "state"  -> convert to int
+    // get the value of second no from the internal component "state"  -> convert to int
+    // get the operation from the internal component "state"
+
+    // perform calculation
+    // set the calculated value on the internal component "state" 
+  };
+
+  return (
+    <div className='calculator-container'>
+      <div className='row'>
+        <input type="text" name="" placeholder="enter the 1st no" defaultValue={props.firstNo}/>
+        <select name="">
+          <option>+</option>
+          <option>-</option>
+          <option>*</option>
+        </select>
+        <input type="text" name="" placeholder="enter the 2nd no" defaultValue={props.secondNo}/>
+      </div>
+      <div className='row'>
+        <button onClick={handleClick}>Calculate</button>
+      </div>
+      <div className='row'>
+        <input type="text" name="" placeholder="here is the ans!" defaultValue={props.ans} readOnly/>
+      </div>
+    </div>
+  )
+} */
+
+export default CalculatorForm;
